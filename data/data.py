11d7436822b742f391073cc262ec224c14347296
Joao Pedro <joaopedrombvboas@gmail.com>
2018-09-26 09:06:32 -0300

    first commit

---
+++ data/data.py
@@ -0,0 +1,20 @@
+def Topics():
+    
+    topics = [
+        {
+            'id': 1,
+            'title': "Titulo",
+            'type':"Jusbrasil"
+        },
+        {
+            'id': 2,
+            'title': "Vaga",
+            'type':"Desenvolvendor na Jusbrasil"
+        },
+        {
+            'id': 3,
+            'title': "Front End",
+            'type':"CSS"
+        },
+    ]
+    return topics
